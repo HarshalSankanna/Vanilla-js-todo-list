7954e8f18ce314dbc0fb78b11c3135290d060d35
Harshal <harshal0921@gmail.com>
2023-12-09 13:47:52 +0530

    did the functionality

---
+++ script.js
@@ -0,0 +1,24 @@
+const ul = document.querySelector("ul");
+const input = document.querySelector("input");
+const button = document.querySelector("button");
+
+const addTask = () => {
+  const inputValue = input.value;
+  input.value = "";
+  const task = document.createElement("li");
+  const span = document.createElement("span");
+  const delBtn = document.createElement("button");
+  task.appendChild(span);
+  span.textContent = inputValue;
+  task.appendChild(delBtn);
+  delBtn.textContent = "Delete";
+  ul.appendChild(task);
+  //to delete a task
+  delBtn.addEventListener("click", () => {
+    ul.removeChild(task);
+  });
+
+  input.focus();
+};
+
+button.addEventListener("click", addTask);
